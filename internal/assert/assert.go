d6ca2eee3c085539d8522018c2d57ea7f29a081c
LidoHon <lidohon10@gmail.com>
2024-11-30 11:46:55 +0300

    integration testing

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -19,3 +19,11 @@ func StringContains(t *testing.T, actual, expectedSubstring string){
 	}
 }
 
+func NilError(t *testing.T, actual error){
+	t.Helper()
+
+	if actual !=nil{
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
+
